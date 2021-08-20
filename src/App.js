e50999ee4ce2be81d568838c43fd7f2885883558
rcalvs <rafaelcalvette@gmail.com>
2021-08-20 15:38:50 -0300

    instaling craco and tailwind

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="bg-blue-800">
+        <p className="text-white">
+          Aqui começa.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
