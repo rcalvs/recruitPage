9568324189e06703ea17dd48b205ea7ba19c4f50
rcalvs <rafaelcalvette@gmail.com>
2021-08-20 16:37:04 -0300

    Tailwind Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
+import Banner from './components/Banner';
+// import Form from './components/Form';
 import Header from './components/Header';
+import Section from './components/Section';
+import HeaderImg from './imgs/investors_xl_lg.jpg';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main className="bg-blue-800">
+      <div className="bg-blue-800">
         <p className="text-white">
           Aqui é o Logo.
         </p>
-      </main>
+      <img src={HeaderImg} alt="Header Img"/>
+      </div>
+      <Banner />
+      <Section />
+      {/* <Form /> */}
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -71,9 +71,9 @@ const resources = [
   { name: 'Security', description: 'Understand how we take your privacy seriously.', href: '#', icon: ShieldCheckIcon },
 ]
 const recentPosts = [
-  { id: 1, name: 'Boost your conversion rate', href: '#' },
-  { id: 2, name: 'How to use search engine optimization to drive traffic to your site', href: '#' },
-  { id: 3, name: 'Improve your customer experience', href: '#' },
+  { id: 1, name: 'Boost your conversion rate', href: '/' },
+  { id: 2, name: 'How to use search engine optimization to drive traffic to your site', href: '/' },
+  { id: 3, name: 'Improve your customer experience', href: '/' },
 ]
 
 function classNames(...classes) {
@@ -86,7 +86,7 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
+            <a href="/">
               <span className="sr-only">Workflow</span>
               <img
                 className="h-8 w-auto sm:h-10"
@@ -167,10 +167,10 @@ export default function Header() {
               )}
             </Popover>
 
-            <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Pricing
             </a>
-            <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Docs
             </a>
 
@@ -233,7 +233,7 @@ export default function Header() {
                             </ul>
                           </div>
                           <div className="mt-5 text-sm">
-                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                            <a href="/" className="font-medium text-indigo-600 hover:text-indigo-500">
                               {' '}
                               View all posts <span aria-hidden="true">&rarr;</span>
                             </a>
@@ -247,11 +247,11 @@ export default function Header() {
             </Popover>
           </Popover.Group>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+            <a href="/" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
               Sign in
             </a>
             <a
-              href="#"
+              href="/"
               className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
             >
               Sign up
@@ -304,11 +304,11 @@ export default function Header() {
             </div>
             <div className="py-6 px-5 space-y-6">
               <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
                   Pricing
                 </a>
 
-                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
                   Docs
                 </a>
                 {resources.map((item) => (
@@ -323,14 +323,14 @@ export default function Header() {
               </div>
               <div>
                 <a
-                  href="#"
+                  href="/"
                   className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                 >
                   Sign up
                 </a>
                 <p className="mt-6 text-center text-base font-medium text-gray-500">
                   Existing customer?{' '}
-                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
+                  <a href="/" className="text-indigo-600 hover:text-indigo-500">
                     Sign in
                   </a>
                 </p>
