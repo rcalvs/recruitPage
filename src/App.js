35e82a876d402761dce1602e7291fd424f800e80
rcalvs <rafaelcalvette@gmail.com>
2021-08-23 16:23:07 -0300

    rotas e inicio do Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+import { Switch, Route } from "react-router-dom";
 import './App.css';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
-// import Form from './components/Form';
+import Form from './components/Form';
 import Header from './components/Header';
 import Section from './components/Section';
 import HeaderImg from './imgs/investors_xl_lg.jpg';
 
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="bg-blue-800">
-        <img src={HeaderImg} alt="Header Img"/>
-      </div>
-      <Banner />
-      <Section />
-      {/* <Form /> */}
-      <Footer />
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <div className="App">
+          <Header />
+          <div className="bg-blue-800">
+            <img src={HeaderImg} alt="Header Img"/>
+          </div>
+          <Banner />
+          <Section />
+          <Footer />
+         </div>
+      </Route>
+      <Route path="/cadastro">
+        <div className="App">
+          <Header />
+          <Form />
+          <Footer />
+         </div>
+      </Route>
+    </Switch>
+    
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -159,7 +159,7 @@ export default function Header() {
             </Popover>
 
             <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
-              Para você: Empresa
+              Para você
             </a>
             <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Contato
