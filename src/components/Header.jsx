52e7f8974de6d1fc2abcc2030fe50d6fc459682d
rcalvs <rafaelcalvette@gmail.com>
2021-08-29 14:40:34 -0300

    Setting correct routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -75,7 +75,7 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="/">
+            <a href="/recruitPage/#">
               <span className="sr-only">Workflow</span>
               <img
                 className="h-8 w-auto sm:h-10"
@@ -156,10 +156,10 @@ export default function Header() {
               )}
             </Popover>
 
-            <a href="/#/cadastro" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <a href="/recruitPage/#/cadastro" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Para você
             </a>
-            <a href="/#/cadastro" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <a href="/recruitPage/#/cadastro" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Contato
             </a>
 
@@ -216,11 +216,11 @@ export default function Header() {
             </Popover>
           </Popover.Group>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <a href="/#/cadastro" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+            <a href="/recruitPage/#/cadastro" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
               Login
             </a>
             <a
-              href="/#/cadastro"
+              href="/recruitPage/#/cadastro"
               className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
             >
               Cadastre-se
