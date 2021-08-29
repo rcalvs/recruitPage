883ce9d1f692cefa49d90e91576c4c1a25c073e3
rcalvs <rafaelcalvette@gmail.com>
2021-08-29 14:38:43 -0300

    Routes deu certo o//

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -156,10 +156,10 @@ export default function Header() {
               )}
             </Popover>
 
-            <a href="/cadastro" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <a href="/#/cadastro" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Para você
             </a>
-            <a href="/cadastro" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <a href="/#/cadastro" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Contato
             </a>
 
@@ -216,11 +216,11 @@ export default function Header() {
             </Popover>
           </Popover.Group>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <a href="/cadastro" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+            <a href="/#/cadastro" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
               Login
             </a>
             <a
-              href="/cadastro"
+              href="/#/cadastro"
               className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
             >
               Cadastre-se
